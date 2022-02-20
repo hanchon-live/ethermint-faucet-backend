86103292032bdecd43d978c19e54c629d59624e1
coreyxx <corey1992@gmail.com>
2022-02-20 22:05:33 +0800

    The sed command is compatible with multiple operating systems

--- supportUptick.sh
+++ supportUptick.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 file='./node_modules/@hanchon/ethermint-address-converter/lib/converter.js'
-sed -i "" "s/ethm/uptick/g" $file
+if [[ `uname  -a` =~ "Darwin" ]];then
+    sed -i "" "s/ethm/uptick/g" $file
+else
+    sed -i  "s/ethm/uptick/g" $file
+fi
